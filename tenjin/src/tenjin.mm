5f63acd5452f5a747830dbaa77a2cef4e2c6815f
MaratGilyazov <maratai@yandex.ru>
2020-02-25 18:49:35 +0500

    fix bugs

--- tenjin/src/tenjin.mm
+++ tenjin/src/tenjin.mm
@@ -4,7 +4,7 @@
 #include "tenjin.h"
 #include "TenjinSDK.h"
 
-void Tenjin_Init(const char*api_key, bool gdpr_consent) {
+void Tenjin_Init(const char* api_key, bool gdpr_consent) {
     
     [TenjinSDK init:[NSString stringWithUTF8String:api_key]];
 
@@ -17,12 +17,12 @@ void Tenjin_Init(const char*api_key, bool gdpr_consent) {
     [TenjinSDK connect];
 }
 
-void Tenjin_CustomEvent(const char*event_name) {
-    [TenjinSDK sendEventWithName:event_name]; 
+void Tenjin_CustomEvent(const char* event_name) {
+    [TenjinSDK sendEventWithName:[NSString stringWithUTF8String:event_name]]; 
 }
 
-void Tenjin_CustomEventWithValue(const char*event_name, const char*event_value) {
-    [TenjinSDK sendEventWithName:event_name andEventValue:event_value]; 
+void Tenjin_CustomEventWithValue(const char* event_name, const char* event_value) {
+    [TenjinSDK sendEventWithName:[NSString stringWithUTF8String:event_name] andEventValue:[NSString stringWithUTF8String:event_value]]; 
 }
 
 #endif
